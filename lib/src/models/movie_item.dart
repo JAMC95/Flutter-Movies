d21ecfb36c581dbe01574459bf14eb8ddf6413a7
joseantoniomoralcastilla@gmail.com <joseantoniomoralcastilla@gmail.com>
2019-09-05 22:09:08 +0200

    Implementado el modelo movie, y dándole uso en el provider

---
+++ lib/src/models/movie_item.dart
@@ -0,0 +1,127 @@
+// To parse this JSON data, do
+//
+//     final movieItem = movieItemFromJson(jsonString);
+
+import 'dart:convert';
+
+MovieItem movieItemFromJson(String str) => MovieItem.fromJson(json.decode(str));
+
+String movieItemToJson(MovieItem data) => json.encode(data.toJson());
+
+class MovieItem {
+  int page;
+  int totalResults;
+  int totalPages;
+  List<Result> results;
+
+  MovieItem({
+    this.page,
+    this.totalResults,
+    this.totalPages,
+    this.results,
+  });
+
+  factory MovieItem.fromJson(Map<String, dynamic> json) => new MovieItem(
+    page: json["page"],
+    totalResults: json["total_results"],
+    totalPages: json["total_pages"],
+    results: new List<Result>.from(json["results"].map((x) => Result.fromJson(x))),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "page": page,
+    "total_results": totalResults,
+    "total_pages": totalPages,
+    "results": new List<dynamic>.from(results.map((x) => x.toJson())),
+  };
+}
+
+class Result {
+  double popularity;
+  int voteCount;
+  bool video;
+  String posterPath;
+  int id;
+  bool adult;
+  String backdropPath;
+  OriginalLanguage originalLanguage;
+  String originalTitle;
+  List<int> genreIds;
+  String title;
+  double voteAverage;
+  String overview;
+  DateTime releaseDate;
+
+  Result({
+    this.popularity,
+    this.voteCount,
+    this.video,
+    this.posterPath,
+    this.id,
+    this.adult,
+    this.backdropPath,
+    this.originalLanguage,
+    this.originalTitle,
+    this.genreIds,
+    this.title,
+    this.voteAverage,
+    this.overview,
+    this.releaseDate,
+  });
+
+  factory Result.fromJson(Map<String, dynamic> json) => new Result(
+    popularity: json["popularity"].toDouble(),
+    voteCount: json["vote_count"],
+    video: json["video"],
+    posterPath: json["poster_path"],
+    id: json["id"],
+    adult: json["adult"],
+    backdropPath: json["backdrop_path"],
+    originalLanguage: originalLanguageValues.map[json["original_language"]],
+    originalTitle: json["original_title"],
+    genreIds: new List<int>.from(json["genre_ids"].map((x) => x)),
+    title: json["title"],
+    voteAverage: json["vote_average"].toDouble(),
+    overview: json["overview"],
+    releaseDate: DateTime.parse(json["release_date"]),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "popularity": popularity,
+    "vote_count": voteCount,
+    "video": video,
+    "poster_path": posterPath,
+    "id": id,
+    "adult": adult,
+    "backdrop_path": backdropPath,
+    "original_language": originalLanguageValues.reverse[originalLanguage],
+    "original_title": originalTitle,
+    "genre_ids": new List<dynamic>.from(genreIds.map((x) => x)),
+    "title": title,
+    "vote_average": voteAverage,
+    "overview": overview,
+    "release_date": "${releaseDate.year.toString().padLeft(4, '0')}-${releaseDate.month.toString().padLeft(2, '0')}-${releaseDate.day.toString().padLeft(2, '0')}",
+  };
+}
+
+enum OriginalLanguage { EN, TL, JA }
+
+final originalLanguageValues = new EnumValues({
+  "en": OriginalLanguage.EN,
+  "ja": OriginalLanguage.JA,
+  "tl": OriginalLanguage.TL
+});
+
+class EnumValues<T> {
+  Map<String, T> map;
+  Map<T, String> reverseMap;
+
+  EnumValues(this.map);
+
+  Map<T, String> get reverse {
+    if (reverseMap == null) {
+      reverseMap = map.map((k, v) => new MapEntry(v, k));
+    }
+    return reverseMap;
+  }
+}
--- lib/src/resources/movie_api_provider.dart
+++ lib/src/resources/movie_api_provider.dart
@@ -1,1 +1,22 @@
-import 'package:http/http.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:movies_app/src/models/movie_item.dart';
+
+class MovieApiProvider {
+  final _apiKey = "e89f9f7cbba267b8c7025fc06a1bb120";
+  final _baseUrl = "https://api.themoviedb.org/3";
+
+  Future<MovieItem> getMovieList() async{
+    final urlPopularMovies = '${_baseUrl}/movie/popular?api_key=${_apiKey}';
+
+    final response = await http.get(urlPopularMovies);
+
+    if(response.statusCode == 200) {
+      MovieItem.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Fallo al obetener el listado de películas');
+    }
+  }
+
+}
+
